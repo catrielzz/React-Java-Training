617e6fda342ace3efd4c3451d0bb6af3e6378975
Yoel.bb <ye@bb.vision>
2020-12-22 14:54:11 -0300

    first commit

---
+++ springboot-backend/src/main/java/com/yoelestrada/springboot/model/wea.java
@@ -0,0 +1,4 @@
+package com.yoelestrada.springboot.model;
+
+public class wea {
+}
---
+++ springboot-backend/src/test/java/com/yoelestrada/springboot/SpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoelestrada.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
